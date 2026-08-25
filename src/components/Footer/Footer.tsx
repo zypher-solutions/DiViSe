21408b3d1c0c024b8adec1a03b0475df5fb21ded
Muhammed Hasheem <hasheemhsm4@gmail.com>
2026-08-25 19:30:08 +0530

    Refactor Footer for flawless mobile responsiveness and update email label

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,14 @@ import './Footer.css';
 const Footer: React.FC = () => {
   return (
     <footer className="footer-section">
-      <div className="footer-top">
-        <div className="footer-top-left">
-          {/* Icon removed as requested */}
+      <div className="footer-content">
+        
+        <div className="footer-brand">
+          <img src="/footer-submarine-icon.png" alt="DIVISE Logo" className="footer-brand-icon" />
+          <h1 className="footer-giant-text">DIVISE</h1>
         </div>
 
-        <div className="footer-top-right">
+        <div className="footer-links-container">
           <div className="footer-col">
             <h4>USEFUL</h4>
             <a href="#">Home</a>
@@ -25,16 +27,10 @@ const Footer: React.FC = () => {
           <div className="footer-col">
             <h4>UPDATES</h4>
             <a href="https://www.linkedin.com/in/commander-dilip-jain-retd-b8a506268/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <a href="mailto:dilipjain@gmail.com">dilipjain@gmail.com</a>
+            <a href="mailto:dilipjain@gmail.com">Email</a>
           </div>
         </div>
-      </div>
 
-      <div className="footer-bottom">
-        <div className="footer-bottom-brand">
-          <img src="/footer-submarine-icon.png" alt="DIVISE Logo" className="footer-bottom-icon" />
-          <h1 className="footer-giant-text">DIVISE</h1>
-        </div>
       </div>
     </footer>
   );
